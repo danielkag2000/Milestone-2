2ed22ddae5e9f17ab13c4ad3349f448023df859d
evyatar18 <evyatar38@gmail.com>
2019-01-10 20:25:20 +0200

    custom client handler

--- server/testClientHandler.cpp
+++ server/testClientHandler.cpp
@@ -1,1 +1,49 @@
 #include "testClientHandler.h"
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+#include "../solve_problem/solver.h"
+
+namespace server_side {
+    class StringReverser : public algorithm::Solver<const string*,string> {
+        string solve(const string* str) {
+            string cpy(*str);
+            reverse(cpy.begin(), cpy.end());
+
+            return cpy;
+        }
+    };
+
+    MyTestClientHandler::MyTestClientHandler() : _solver(new StringReverser()) {}
+
+    void MyTestClientHandler::handleClient(server_side::Client client) {
+        vector<char> vec;
+
+        vec.resize(20);
+        ssize_t read = client.read(vec, 20);
+
+        auto it = vec.begin();
+        while (read-- > 0) {
+            cout << *it;
+            it++;
+        }
+
+        cout << endl;
+
+        client.write(vec, 20);
+
+        cout << "wrote to client" << endl;
+    }
+
+    void MyTestClientHandler::handleClient(istream &is, ostream &os) {
+        string x;
+
+        getline(is, x);
+
+        cout << "sending: " << x << endl;
+        os << x;
+    }
+}
--- server/testClientHandler.h
+++ server/testClientHandler.h
@@ -2,13 +2,22 @@
 #define MILESTONE_2_TESTCLIENTHANDLER_H
 
 #include "clientHandler.h"
+#include "../solve_problem/solver.h"
 
 namespace server_side {
     class MyTestClientHandler : public ClientHandler {
+    private:
+        algorithm::Solver<const string*, string>* _solver;
+    public:
+        MyTestClientHandler();
+
+        virtual void handleClient(Client client);
 
         virtual void handleClient(istream& is, ostream& os);
 
-        virtual ~MyTestClientHandler();
+        virtual ClientHandler* copy() {
+            return new MyTestClientHandler;
+        }
     };
 }
 
