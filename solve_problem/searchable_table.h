4ee9faf3f784c29159309029dff4f295bd471f7e
danielk <daniel2000@users.noreply.github.com>
2019-01-12 16:30:27 +0200

    add the searchable table class for using in the input of the user

---
+++ solve_problem/searchable_table.h
@@ -0,0 +1,39 @@
+#ifndef MILESTONE_2_SEARCHABLE_TABLE_H
+#define MILESTONE_2_SEARCHABLE_TABLE_H
+
+#include <vector>
+#include "searchable.h"
+#include "state.h"
+
+using namespace std;
+
+typedef pair<int, int> pInt;
+
+class SearchableTable : public Searchable<pInt> {
+    int size;
+    vector<vector<int>> table;
+    State<pInt> start;
+    State<pInt> end;
+
+public:
+
+    SearchableTable(vector<vector<int>> table, pInt startPoint, pInt endPoint) {
+        this->table = table;
+        this->size = table.size();
+        this->start = State<pInt>(startPoint);
+        this->start.setCost(table[0][0]);
+        this->end = State<pInt>(endPoint);
+    }
+
+    State<pInt> getInitialState() {
+        return start;
+    }
+
+    State<pInt> getGoalState() {
+        return end;
+    }
+
+    virtual list<State<pInt>> getAllPossibleStates(const State<pInt>& s);
+};
+
+#endif
